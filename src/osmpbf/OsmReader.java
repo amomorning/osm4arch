51dcdb16d5e8637cbbe35c0816565eca9889f42c
Yichen Mo <amomorning@gmail.com>
2019-10-23 13:25:57 +0800

    update...

--- src/osmpbf/OsmReader.java
+++ src/osmpbf/OsmReader.java
@@ -11,6 +11,7 @@ import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.NodeContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.RelationContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.WayContainer;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
 import org.openstreetmap.osmosis.core.domain.v0_6.Way;
 import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
@@ -20,7 +21,7 @@ import crosby.binary.osmosis.OsmosisReader;
 
 
 public class OsmReader implements Sink {
-	 int cnt = 0;
+	int cnt;
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
@@ -33,7 +34,7 @@ public class OsmReader implements Sink {
 	@Override
 	public void initialize(Map<String, Object> arg0) {
 		// TODO Auto-generated method stub
-		
+		cnt = 0;
 	}
 
 	@Override
@@ -54,22 +55,31 @@ public class OsmReader implements Sink {
 		// TODO Auto-generated method stub
         if (entityContainer instanceof NodeContainer) {
             // Nothing to do here
-        } else if (entityContainer instanceof WayContainer) {
-            Way myWay = ((WayContainer) entityContainer).getEntity();
-            for (Tag myTag : myWay.getTags()) {
-                if ("building".equalsIgnoreCase(myTag.getKey())) {
-                	if("house".equalsIgnoreCase(myTag.getValue())) {
-                		cnt ++;
-                     System.out.println("It's a apartment: " + myWay.getId());
-                    
-                	List<WayNode> nodes = myWay.getWayNodes();
-                	for(WayNode node:nodes) {
+        	Node myNode = ((NodeContainer)entityContainer).getEntity();
+        	System.out.println("node = " + myNode.getId() + "lat = " + myNode.getLatitude() + " lon = " + myNode.getLongitude()) ;
 
-                		System.out.println("lat = " + node.getLatitude() + " lon = " + node.getLongitude());
-                	}
-                	}
-                }
-            }
+//			System.out.println();
+        } else if (entityContainer instanceof WayContainer) {
+//            Way myWay = ((WayContainer) entityContainer).getEntity();
+//            boolean building = false;
+//            for (Tag myTag : myWay.getTags()) {
+//                if ("building".equalsIgnoreCase(myTag.getKey())) {
+//                	if("house".equalsIgnoreCase(myTag.getValue())) {
+//                		cnt ++;
+//                     System.out.println("It's a apartment: " + myWay.getId());
+//                    
+//                	List<WayNode> nodes = myWay.getWayNodes();
+//                	for(WayNode node:nodes) {
+//                		System.out.println("nodeid = " + node.getNodeId());
+////                		System.out.println("lat = " + node.getLatitude() + " lon = " + node.getLongitude());
+//                	}
+//                	}
+//                	building = true;
+//                }
+//            }
+//            if(building) {
+//            	
+//            }
         } else if (entityContainer instanceof RelationContainer) {
             // Nothing to do here
         } else {
