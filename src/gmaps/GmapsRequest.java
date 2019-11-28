98b3a98e7ef74639d591e75a253549e73db9576c
Yichen Mo <amomorning@gmail.com>
2019-11-28 23:07:37 +0800

    Add grid searching an area

--- src/gmaps/GmapsRequest.java
+++ src/gmaps/GmapsRequest.java
@@ -1,8 +1,12 @@
 package gmaps;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.PlacesApi;
@@ -12,6 +16,7 @@ import com.google.maps.model.PlaceType;
 import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.PlacesSearchResult;
 
+import osm.GeoMath;
 import utils.Container;
 
 /**
@@ -24,8 +29,6 @@ import utils.Container;
 public class GmapsRequest {
 
 	public static void main(String[] args) {
-		GmapsRequest gr = new GmapsRequest();
-		GmapsDb db = new GmapsDb();
 
 		for (PlaceType type : PlaceType.values()) {
 			if (GmapsTypeDetail.map.get(type.toUrlValue()) == null) {
@@ -35,16 +38,43 @@ public class GmapsRequest {
 
 		}
 
-		db.deleteTable();
-		db.createTable();
+//		db.deleteTable();
+//		db.createTable();
 
-		GeoApiContext context = new GeoApiContext.Builder().apiKey(Info.API_KEY).build();
-		Random rand = new Random();
-		for (int i = 0; i < 3; ++i) {
-			LatLng PRATO = new LatLng(43.8830732 + rand.nextDouble() / 1000.0, 11.0897498 + rand.nextDouble() / 1000.0);
-			int tot = gr.searchNearBy(db, context, PRATO);
-			System.out.println("-------------\n" + tot + " at position " + PRATO + "\n--------------");
+		GmapsRequest gr = new GmapsRequest();
+		try {
+			gr.gridSearch(Container.SW_LAT_LNG, Container.NE_LAT_LNG, 50);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+	}
+
+	public void gridSearch(double[] bl, double[] tr, double radius) throws IOException {
+		double[] min = GeoMath.latLngToXY(bl);
+		double[] max = GeoMath.latLngToXY(tr);
+
+		GmapsDb db = new GmapsDb();
+		GeoApiContext context = new GeoApiContext.Builder().apiKey(Info.API_KEY).build();
+
+		File file = new File("./data/number_of_result.txt");
+		FileWriter out = new FileWriter(file);
+
+//		double step = Math.floor(radius * Math.sqrt(2.0));
+		double step = 3000;
+		for (double x = min[0] + step / 2; x < max[0]; x += step) {
+			for (double y = min[1] + step / 2; y < max[1]; y += step) {
+				double[] latlng = GeoMath.xyToLatLng(x, y);
+				LatLng position = new LatLng(latlng[0], latlng[1]);
+				int tot = searchNearBy(db, context, position);
+				System.out.println("( " + x + " " + y + " )");
+				System.out.println("-------------\n" + tot + " at position " + position + "\n--------------");
+				if (y + step < max[1])
+					out.write(tot + ", ");
+			}
+			out.write("\r\n");
+		}
+		out.close();
 	}
 
 	/**
@@ -80,28 +110,37 @@ public class GmapsRequest {
 				total += response.results.length;
 			}
 		} catch (ApiException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return total;
 	}
 
+	/**
+	 * @Function: resultToDb
+	 * @Description: TODO
+	 * @param db
+	 * @param result
+	 *
+	 * @return: void
+	 */
 	private void resultToDb(GmapsDb db, PlacesSearchResult result) {
 		LatLng position = result.geometry.location;
 
 		String name = result.name;
+		Boolean isChinese = false;
+
+		if (isContainChinese(name))
+			isChinese = true;
 		if (name.indexOf("'") != -1)
 			name = name.replace("'", "''");
 
 		String values = "\'" + result.placeId + "\', " + position.lat + ", " + position.lng + ", " + result.rating
-				+ ", " + result.userRatingsTotal + ", \'" + name + "\'";
+				+ ", " + result.userRatingsTotal + "," + isChinese + ", \'" + name + "\'";
 
 		GmapsTypeDetail.Types type = null;
 		String typeDetail = null;
@@ -115,6 +154,40 @@ public class GmapsRequest {
 		values += ", \'" + type + "\'" + ", \'" + typeDetail + "\'";
 
 		db.insertData(values);
+	}
+
+	/**
+	 * @Function: isContainChinese
+	 * @Description: TODO
+	 * @param str
+	 * @return
+	 *
+	 * @return: boolean
+	 */
+	public boolean isContainChinese(String str) {
+		if (str.indexOf("Chinese") != -1)
+			return true;
+		if (str.indexOf("chinese") != -1)
+			return true;
+		if (str.indexOf("China") != -1)
+			return true;
+		if (str.indexOf("china") != -1)
+			return true;
+		if (str.indexOf("Cinese") != -1)
+			return true;
+		if (str.indexOf("cinese") != -1)
+			return true;
+		if (str.indexOf("Cina") != -1)
+			return true;
+		if (str.indexOf("cina") != -1)
+			return true;
+
+		Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+		Matcher m = p.matcher(str);
+		if (m.find()) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/gmaps/test.java
+++ src/gmaps/test.java
@@ -1,130 +1,17 @@
 package gmaps;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
-import com.google.maps.GeoApiContext;
-import com.google.maps.PlacesApi;
-import com.google.maps.model.LatLng;
-import com.google.maps.model.PlacesSearchResponse;
-
+import osm.GeoMath;
 import utils.Container;
 
 public class test {
             
-    
-    public static String COLUMNS[][] = {
-            {"placeid", "char(255)"}, 
-            {"lat", "float"},
-            {"lng", "float"},
-            {"name", "char(255)"},
-            {"type", "char(255)"}
-            };
-    public static int NOT_NULL_LENGTH = 3;
-    public static String TABLENAME = "function";
 
     public static void main(String[] args) throws Exception {
-        test testCase = new test();
-
-        Connection c = testCase.connectSQL();
-//        testCase.createTable(c);
-//        testCase.deleteTable(c);
-
-        testCase.testGoogleAPI(c);
+        double[] xy = GeoMath.latLngToXY(Container.MAP_LAT_LNG);
+        double[] latLng = GeoMath.xyToLatLng(xy[0], xy[1]);
+        System.out.println(latLng[0] + " " + latLng[1]);
     }
 
-    public Connection connectSQL() {
 
-        try {
-            Connection c = null;
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/prato", "postgres", "1218");
-            System.out.println("Opened database successfully");
-            return c;
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-            return null;
-        }
-    }
-
-    public void createTable(Connection c) {
-        Statement stmt = null;
-        try {
-
-            stmt = c.createStatement();
-            String sql = "create table " + TABLENAME + " (";
-            for(int i = 0; i < COLUMNS.length; ++ i) {
-                sql += COLUMNS[i][0] + " "+ COLUMNS[i][1];
-                if(i == 0) sql += "primary key";
-                if(i < NOT_NULL_LENGTH) sql += " not null";
-                if(i != COLUMNS.length-1) sql += ", ";
-                else sql += ")";
-            }
-            stmt.executeUpdate(sql);
-            stmt.close();
-            System.out.println("create table successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-
-    }
-    
-    public void deleteTable(Connection c) {
-        
-        Statement stmt = null;
-        try {
-
-            stmt = c.createStatement();
-            String sql = "DROP TABLE " + TABLENAME;
-            stmt.executeUpdate(sql);
-            stmt.close();
-            System.out.println("delete table successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-    
-    public void insertData(Connection c, String values) {
-        Statement stmt = null;
-        try {
-
-            stmt = c.createStatement();
-            String sql = "insert into " + TABLENAME + " (";
-            for(int i = 0; i < COLUMNS.length; ++ i) {
-                sql += COLUMNS[i][0];
-                if(i == COLUMNS.length - 1) sql += ");";
-                else sql += ", ";
-            }
-            sql += "values (" + values + ");";
-             
-            stmt.executeUpdate(sql);
-            stmt.close();
-            System.out.println("records created successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-
-    public void testGoogleAPI(Connection c) throws Exception {
-
-        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyB5hXZRD8xQP4NCHODHCX9torCcH1ClMk0").build();
-        LatLng PRATO = new LatLng(Container.MAP_LAT_LNG[0], Container.MAP_LAT_LNG[1]);
-
-        PlacesSearchResponse response = PlacesApi.nearbySearchQuery(context, PRATO).radius(50).await();
-        String value = "";
-        LatLng position = response.results[0].geometry.location;
-        value += position.lat;
-        String[] type = response.results[0].types;
-        
-    }
 
 }
